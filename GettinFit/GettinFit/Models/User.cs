355eb85e5882f7a0b8b2c79d22014d250741272e
Michele Rawlins <shellbop@att.net>
2020-12-01 21:05:46 -0600

    added model and repo for user

--- GettinFit/GettinFit/Models/User.cs
+++ GettinFit/GettinFit/Models/User.cs
@@ -7,5 +7,21 @@ namespace GettinFit.Models
 {
     public class User
     {
+
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Sex { get; set; }
+        public int Height { get; set; }
+        public int CurrentWeight { get; set; }
+        public int Age { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime Birthdate { get; set; }
+        public int CalorieGoal {get; set;}
+        public int WeightGoal { get; set; }
+        public int BeginningWeight { get; set; }
+        public DateTime DateCreated { get; set; }
+     
     }
 }
