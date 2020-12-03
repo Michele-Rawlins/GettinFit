61f060460611f64e1abe6cd6797301da9d709e64
Michele Rawlins <shellbop@att.net>
2020-12-02 18:13:33 -0600

    added model

--- GettinFit/GettinFit/Models/Meal.cs
+++ GettinFit/GettinFit/Models/Meal.cs
@@ -7,5 +7,22 @@ namespace GettinFit.Models
 {
     public class Meal
     {
+        public int MealId { get; set; }
+        public string FoodDescription { get; set; }
+        public int CalorieCount { get; set; }
+
+        public WhichMeal MealType { get; set; }
+        public DateTime Date { get; set; }
+        public int UserId { get; set; }
+
+
+    }
+
+    enum WhichMeal
+    {
+        Breakfast,
+        Lunch,
+        Dinner,
+        Snack
     }
 }
