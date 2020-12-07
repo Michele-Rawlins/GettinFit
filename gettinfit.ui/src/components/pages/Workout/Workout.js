6b441906105ba59510af191e827ee9b6dd69edfd
Michele Rawlins <shellbop@att.net>
2020-12-07 13:01:25 -0600

    wip

---
+++ gettinfit.ui/src/components/pages/Workout/Workout.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Workout extends React.Component {
+  
+
+  render() {
+   
+    
+
+    return (
+     
+
+   <div className="Workout container">
+      <h1 className ="title">Welcome to Your Workout Page</h1>
+   
+  <div className = "aboutUs">
+    <h2 className = "aboutUs">About Us</h2>
+  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+  </div>
+  </div>
+         
+    );   
+    }
+  }
+  
+  export default Home;
